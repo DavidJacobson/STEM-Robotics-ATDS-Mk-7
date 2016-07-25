acf470f9b12946a008ca80b184c857291ca7388a
David Jacobson <david@davidej.com>
2016-07-25 10:33:48 -0400

    god help

--- robot.py
+++ robot.py
@@ -3,12 +3,18 @@ import time
 from servo import Servo
 
 
-
 GPIO.setmode(GPIO.BOARD)
 
 class Robot:
 	def __init__(self, name):
 		self.name = name
 		self.sonic_sensor_servo = Servo(11)
-reggie = Robot('reggie')
-reggie.sonic_sensor_servo.turn(50000)
+	def stop(self):
+		self.sonic_sensor_servo.stop()
+
+try:
+	reggie = Robot('reggie')
+	reggie.sonic_sensor_servo.turn(50000)
+except KeyboardInterrupt:
+	reggie.stop()
+	GPIO.cleanup()
--- servo.py
+++ servo.py
@@ -1,14 +1,19 @@
 import time
 import RPi.GPIO as GPIO
-from RPIO import PWM
 
 class Servo:
 	def __init__(self, pin_number):
-		self.pin_number = pin_number
+		self.pin_number = pin_number	
 		GPIO.setup(pin_number, GPIO.OUT)
-		self.p = PWM.Servo()
-		self.p.set_servo(pin_number, 1200)
+		self.p = GPIO.PWM(pin_number, 50)
 	def turn(self, times):
-		GPIO.setup(self.pin_number, GPIO.OUT)
-		for _ in range(times - 1):
-			GPIO.output(self.pin_number, GPIO.HIGH)
+	#	for _ in range(times - 1):
+		while True:	
+			print "maybe now"
+			self.p.ChangeDutyCycle(7.5)
+			time.sleep(10)
+			print "make it stop"
+			self.p.ChangeDutyCycle(2.5)
+			time.sleep(10)
+	def stop(self):
+		self.p.stop()
