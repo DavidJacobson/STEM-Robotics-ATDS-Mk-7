9a976741438a934e812290030bf0b896ed08a14d
David Jacobson <david@davidej.com>
2016-07-25 17:05:47 -0400

    let the bodies hit the floor

--- robot.py
+++ robot.py
@@ -5,16 +5,19 @@ from servo import Servo
 
 GPIO.setmode(GPIO.BOARD)
 
+#class Servo
+
+
 class Robot:
 	def __init__(self, name):
 		self.name = name
-		self.sonic_sensor_servo = Servo(11)
+		self.sonic_sensor_servo = Servo(7)
 	def stop(self):
 		self.sonic_sensor_servo.stop()
 
 try:
 	reggie = Robot('reggie')
-	reggie.sonic_sensor_servo.turn(50000)
+	reggie.sonic_sensor_servo.turn()
 except KeyboardInterrupt:
 	reggie.stop()
 	GPIO.cleanup()
--- servo.py
+++ servo.py
@@ -6,14 +6,14 @@ class Servo:
 		self.pin_number = pin_number	
 		GPIO.setup(pin_number, GPIO.OUT)
 		self.p = GPIO.PWM(pin_number, 50)
-	def turn(self, times):
-	#	for _ in range(times - 1):
+		self.p.start(7.5)
+	def turn(self):
 		while True:	
 			print "maybe now"
 			self.p.ChangeDutyCycle(7.5)
-			time.sleep(10)
+			time.sleep(1)
 			print "make it stop"
 			self.p.ChangeDutyCycle(2.5)
-			time.sleep(10)
+			time.sleep(1)
 	def stop(self):
 		self.p.stop()
