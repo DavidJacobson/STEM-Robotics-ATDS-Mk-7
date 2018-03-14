fc196cdf34b6ab6c30585f055623bda87a419ce0
David Jacobson <DavidJacobson@users.noreply.github.com>
2018-03-14 14:19:09 -0400

    Update display.py
    Just remove some blank lines

--- display.py
+++ display.py
@@ -2,9 +2,6 @@
 import time
 import RPi.GPIO as GPIO
 
-
-
-
 class Display:
 
 	def __init__(self, SDI, RCLK, SRCLK):
@@ -18,7 +15,6 @@ class Display:
         	GPIO.output(RCLK, GPIO.LOW)
         	GPIO.output(SRCLK, GPIO.LOW)
 		self.hex_vals = [0x3f,0x06,0x5b,0x4f,0x66,0x6d,0x7d,0x07,0x7f,0x6f] #plz work
-		
 	def reg_shift(self, dat): # Not our code, we can't be blamed...
         	for bit in range(0, 8):
                 	GPIO.output(self.SDI, 0x80 & (dat << bit))
@@ -31,7 +27,3 @@ class Display:
 	def disp_val(self, val):
 		self.reg_shift(self.hex_vals[val])
 		time.sleep(0.1)		
-
-
-	
-
